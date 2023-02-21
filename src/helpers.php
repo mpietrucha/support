85e57952e96a8830d6ac22fd3bc50d6c70b85c54
Michał Pietrucha <michal.pietrucha@off-site.pl>
2023-02-21 12:31:05 +0100

    base64

---
+++ src/helpers.php
@@ -0,0 +1,20 @@
+<?php
+
+use Mpietrucha\Support\Types;
+use Illuminate\Support\Collection;
+
+if (! function_exists('collect_config') && function_exists('config') && function_exists('collect')) {
+    function collect_config(string $key, Collection $default = new Collection): mixed {
+        $value = config($value);
+
+        if (! $value) {
+            return $default;
+        }
+
+        if (! Types::array($value)) {
+            return $value;
+        }
+
+        return collect($value);
+    }
+}
