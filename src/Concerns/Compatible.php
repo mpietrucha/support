253a192f7f016740a3ee79d808b978c791eecbdc
Michał Pietrucha <mpietrucha50@gmail.com>
2026-03-14 14:02:43 +0100

    wip

--- src/Concerns/Compatible.php
+++ src/Concerns/Compatible.php
@@ -6,17 +6,12 @@ trait Compatible
 {
     public static function compatible(mixed ...$arguments): bool
     {
-        /** @phpstan-ignore argument.type */
-        return (bool) static::compatibility(...$arguments);
+        return false;
     }
 
     final public static function incompatible(mixed ...$arguments): bool
     {
+        /** @phpstan-ignore argument.type */
         return ! static::compatible(...$arguments);
-    }
-
-    protected static function compatibility(): mixed
-    {
-        return false;
     }
 }
