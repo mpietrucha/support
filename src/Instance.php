e536a7938c25fb9beb3fcbc67266ae2f0128bbe7
Michał Pietrucha <mpietrucha50@gmail.com>
2026-03-17 19:55:03 +0100

    wip

--- src/Instance.php
+++ src/Instance.php
@@ -17,6 +17,9 @@ use Throwable;
 
 abstract class Instance
 {
+    /**
+     * @return ($class is object ? class-string : null|class-string)
+     */
     public static function namespace(object|string $class, bool $autoload = true): ?string
     {
         if (is_object($class)) {
@@ -32,10 +35,6 @@ abstract class Instance
             $class = static::namespace($class);
         }
 
-        if ($class === null) {
-            return null;
-        }
-
         /** @var null|string */
         $file = Arr::map(
             ClassLoader::getRegisteredLoaders(),
@@ -49,6 +48,9 @@ abstract class Instance
         return Path::canonicalize($file);
     }
 
+    /**
+     * @return ($class is object ? class-string : null|class-string)
+     */
     public static function base(object|string $class): ?string
     {
         $class = static::namespace($class);
