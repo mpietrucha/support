e6e08515bbe933ba32e1229243e739fc4427265e
Michał Pietrucha <mpietrucha50@gmail.com>
2026-04-07 17:42:44 +0200

    wip

--- rector.php
+++ rector.php
@@ -26,6 +26,14 @@ return RectorConfig::configure()
     ])
     ->withSets([
         LaravelSetList::LARAVEL_130,
+        LaravelSetList::LARAVEL_CODE_QUALITY,
+        LaravelSetList::LARAVEL_TYPE_DECLARATIONS,
+        LaravelSetList::LARAVEL_ELOQUENT_MAGIC_METHOD_TO_QUERY_BUILDER,
+        LaravelSetList::LARAVEL_IF_HELPERS,
+        LaravelSetList::LARAVEL_COLLECTION,
+        LaravelSetList::LARAVEL_ARRAYACCESS_TO_METHOD_CALL,
+        LaravelSetList::LARAVEL_ARRAY_STR_FUNCTION_TO_STATIC_CALL,
+        LaravelSetList::LARAVEL_CONTAINER_STRING_TO_FULLY_QUALIFIED_NAME,
     ])
     ->withPreparedSets(
         deadCode: true,
