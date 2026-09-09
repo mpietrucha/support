de80268d8c151fd2a2f1c5b981d5782543d20fac
Michał Pietrucha <mpietrucha50@gmail.com>
2026-09-09 15:41:47 +0200

    wip

--- src/Stringable/Concerns/InteractsWithStringable.php
+++ src/Stringable/Concerns/InteractsWithStringable.php
@@ -5,12 +5,15 @@ namespace Mpietrucha\Support\Stringable\Concerns;
 use Mpietrucha\Support\Str;
 use Mpietrucha\Support\Stringable;
 
+/**
+ * @phpstan-require-extends Stringable
+ */
 trait InteractsWithStringable
 {
-    public function replacePattern(string $pattern, mixed $replacement, ?string $indicator = null): Stringable
+    public function replacePattern(string $pattern, mixed $replacement, ?string $indicator = null): static
     {
         $value = $this->value();
 
-        return Str::replacePattern($pattern, $replacement, $value, $indicator) |> Stringable::make(...);
+        return Str::replacePattern($pattern, $replacement, $value, $indicator) |> static::make(...);
     }
 }
